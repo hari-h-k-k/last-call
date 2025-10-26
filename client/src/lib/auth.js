d1bc5c036415eea49c1c362eb2647c31550754bf
Harikrishnan K <harikdas2001@gmail.com>
2025-10-26 19:43:07 +0530

    client rename

--- client/src/lib/auth.js
+++
@@ -1,9 +0,0 @@
-let authToken = null;
-
-// Setter function called from AuthContext
-export const setAuthToken = (token) => {
-  authToken = token;
-};
-
-// Getter function for Axios interceptor
-export const getAuthToken = () => authToken;
